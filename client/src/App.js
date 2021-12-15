5a278053f34001200f0c1caab3a35bd6d7aec27a
MarkGee45 <moel55.mg@gmail.com>
2021-12-15 11:17:57 -0800

    added dependencies to client and server side"

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    axios.get("/example")
+    .then((res) => console.log(res))
+    .catch(err => console.log(err))
+  }, []);
+
+  return (
+    <div>
+      <h1>Party People</h1>
+    </div>
+  );
+}
+
+export default App;
