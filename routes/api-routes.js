0696ccf705b88c4d4695726496dcd52946d82ea8
seenanadimi <78619195+seenanadimi@users.noreply.github.com>
2022-01-12 14:57:48 -0800

    fix(typo): api-route typo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/example", (req, res) => {
-    res.send("messae from back end: success")
-})
-
+  res.send("message from back end: success");
+});
 
 module.exports = router;
