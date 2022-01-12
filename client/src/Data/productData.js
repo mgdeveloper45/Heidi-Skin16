00594079f9fa307c500e7af2b76ae2923374c070
seenanadimi <78619195+seenanadimi@users.noreply.github.com>
2022-01-12 14:45:14 -0800

    data(product): intial product data

---
+++ client/src/Data/productData.js
@@ -0,0 +1,513 @@
+export const ProductDataArray = [
+  {
+    id: 1,
+    data: {
+      img: null,
+      brand: "Botalab",
+      productName: "Suamel Body Wash",
+      price: "$33.00",
+      size: {
+        capacity: null,
+        ml: "500ml",
+        oz: "116.9 fl.oz",
+      },
+      skinType: null,
+      descriptions: {
+        primaryDescription:
+          "After a day of external stressors and impurities, the dense foam of this body wash will lovingly caress and recharge your distressed skin.",
+        secondaryDescription:
+          "[Skin Brightening & Anti-Wrinkle Dual Function Cosmetic]",
+        disclaimer: "*This is not a medical prevention or treatment",
+      },
+    },
+  },
+  {
+    id: 2,
+    data: {
+      img: null,
+      brand: "Botalab",
+      productName: "Suamel Body Scrub",
+      price: "$26.00",
+      size: {
+        capacity: null,
+        ml: "200g",
+        oz: "7.05 oz",
+      },
+      skinType: null,
+      descriptions: {
+        primaryDescription:
+          "Tigersalt granules work with water to gently melt away dead skin cells, helping to moisten and smoothe your skin. ",
+        secondaryDescription:
+          "[Skin Brightening & Anti-Wrinkle Dual Function Cosmetic]",
+        disclaimer: null,
+      },
+    },
+  },
+  {
+    id: 3,
+    data: {
+      img: null,
+      brand: "Botalab",
+      productName: "Suamel Body Cream",
+      price: "$29.00",
+      size: {
+        capacity: null,
+        ml: "250ml",
+        oz: "8.45 fl.oz.",
+      },
+      skinType: null,
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Smooth creamy texture follows the direction of your skin, absorbs nicely without leaving any residue, and delivers ample moisture and nutrients, leaving you with healthy, glowing skin.",
+          secondaryDescription:
+            "[Skin Brightening & Anti-Wrinkle Dual Function Cosmetic]",
+          disclaimer: null,
+        },
+        secondary: {},
+      },
+    },
+  },
+  {
+    id: 4,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Snow Enzyme Cleanser",
+      price: "$28.00",
+      size: {
+        capacity: "Capacity and weight:  120g (4.23oz.)",
+        ml: "250ml",
+        oz: "8.45 fl.oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Foam with a soft and thick texture like snow cleans the skin refreshingly.",
+          secondaryDescription:
+            "Enzymatic cleanser that makes the skin clear and clean.",
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Gentle and clean, pleasant cleansing",
+          2: "Mild exfoliation is a basic, bromelain enzyme foam that absorbs impurities and supplies moisture to make the skin breathe.",
+          3: "Cleansing clean and cleansing foam at once, a cleansing cleansing foam formula that reduces irritation.",
+          4: "A cleanser that gently cleanses the skin with ingredients from soap berry and coconut and makes the skin as clear and clear as snow.",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 5,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Vallatto Oil Mist",
+      price: "$39.00",
+      size: {
+        capacity: "Capacity and weight: 50ml (1.69 fl. oz.)",
+        ml: "50ml",
+        oz: "1.69 fl.oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Vallatto Oil Mist has been changed to Vieto Oil Mist. Only the name has changed, but everything is the same as the previous version.",
+          secondaryDescription:
+            "A multi-care bi-phase mist that refreshes, nourishes, and control the skin for a soft touch and healthy-looking glow.",
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Hydrating and refreshing",
+          2: "Anti-aging, wrinkle improving",
+          3: "Protection and brightening",
+          4: "Rejuvenating",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 6,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Calming Balance Gel",
+      price: "$39.00",
+      size: {
+        capacity: "Capacity and weight: 50ml (1.69 fl. oz.)",
+        ml: "50ml",
+        oz: "1.69 fl.oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Calming Balance Gel has special ingredients to instantly calm your skin and maintain the skin's healthy Ph balance.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Maintain your skin's healthy pH balance",
+          2: "Helps to balance the skin oil/moisture level",
+          3: "Protects the skin from the harmful environment",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 7,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Active Clean-Up Powder",
+      price: "$33.00",
+      size: {
+        capacity: "Capacity and weight: 90g (3.17 oz.)",
+        grams: "90g",
+        oz: "1.69 fl.oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Removes skin impurities and contains plenty of moisture to clean skin.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Hydrolyzed Collagen makes the skin elastic and soft",
+          2: "Skin is not stripped of moisture and oil after cleansing",
+          3: "Safe for sensitive skin",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 8,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "4D Lusture Cushion",
+      price: "$55.00",
+      size: {
+        capacity: "Capacity and weight: 15g (0.52 oz.) *2ea",
+        grams: "15g",
+        oz: "0.52 oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "With a radiant formula that brightens the skin tone, it creates natural radiant skin from any angle.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "UV Protection",
+          2: "Wrinkle improvement triple functionality",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 9,
+    data: {
+      img: null,
+      brand: "Botalab",
+      productName: "Deserticola Treatment Conditioner",
+      price: "$30.00",
+      size: {
+        capacity: "Capacity and weight: 300ml (10.12 fl.oz.)",
+        ml: "300ml",
+        oz: "10.14 fl.oz.",
+      },
+      skinType: null,
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "After shampoo, apply directly to the scalp. Then massage treatment evenly through out your entire scalp for about 1 minute. As you rinse, work the lather from top of your head to the end of your hair.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {},
+      },
+    },
+  },
+  {
+    id: 10,
+    data: {
+      img: null,
+      brand: "Botalab",
+      productName: "Deserticola Shampoo",
+      price: "$36.00",
+      size: {
+        capacity: "Capacity and weight: 500ml (16.9 fl.oz.)",
+        ml: "500ml",
+        oz: "16.9 fl.oz.",
+      },
+      skinType: null,
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Deserticola, which has the energy and vitality of strong microalgae that grows in the desert, has been developed for 25 years and helps you to create rich and beautiful hair. Contains amino acid-based cleaning ingredients and natural extracts.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "It makes fine and rich bubbles to cleanly and gently cleanse the waste accumulated on the scalp.",
+        },
+      },
+    },
+  },
+  {
+    id: 11,
+    data: {
+      img: null,
+      brand: "Botalab",
+      productName: "Deserticola Hair Care Set",
+      price: "$66.00",
+      set: {
+        shampoo: {
+          size: {
+            ml: "500ml",
+            oz: "16.9 fl.oz.",
+          },
+          conditioner: {
+            size: {
+              ml: "300ml",
+              oz: "10.14 fl.oz.",
+            },
+          },
+        },
+      },
+      skinType: null,
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Botalab Deserticola Hair Care Set includes Shampoo and Conditioner.  Both products were created with the technology acquired from Korea Research Institute of Bioscience and Biotechnology. Key ingredient is Deserticola.  It provides moisture and oil to reverse damage and cares for the health of the entire scalp and hair.",
+          secondaryDescription:
+            "Hair care Shampoo & Conditioner to relieve hair loss symptoms.",
+          disclaimer: {
+            1: "NOT a drug / treatment for the prevention and treatment of hair loss from disease(s).",
+            2: "*BNIS: Botalab's Negative Ingredient List System",
+          },
+        },
+        secondary: {
+          1: "Deserticola, which has the energy and vitality of strong microalgae that grows in the desert, has been developed for 25 years and helps you to create rich and beautiful hair.",
+          2: "Contains amino acid-based cleaning ingredients and natural extracts.",
+          3: "It makes fine and rich bubbles to cleanly and gently cleanse the waste accumulated on the scalp.",
+          4: "Using carefully selected materials that passed BNIS, harmful ingredients that may irritate the scalp and hair are excluded as much as possible.",
+        },
+      },
+    },
+  },
+  {
+    id: 12,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Dermatology First Package EX",
+      price: "$99.00",
+      set: {
+        capacity:
+          "Capacity and weight:  Booster: 120ml (4.05oz.) / Serum: 45ml (1.52 fl. oz.)",
+        booster: {
+          size: {
+            ml: "120ml",
+            oz: "4.05 oz.",
+          },
+          serum: {
+            size: {
+              ml: "45ml",
+              oz: "1.52 fl.oz.",
+            },
+          },
+        },
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Dermatology First Package EX by Incellderm is a set of strong dual-function treatments (Booster/Toner and Serum) that correct visible signs of aging, and deliver effective skin brightening.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Regenerating and elasticity restoring",
+          2: "Anti-aging, wrinkle improving",
+          3: "Brightening",
+          4: "Rejuvenating",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 13,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Daily Aqua BB",
+      price: "$33.00",
+      capacity: "Capacity and weight: 30g (1.05 oz.) ",
+      size: {
+        g: "30g",
+        oz: "1.05 oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "It fills brightly from the inside of the skin with a permeating BB rather than a covering.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Non sticky refreshing skin shield",
+          2: "Flawless triple skin care",
+          3: "Safe for sensitive skin",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 14,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Aqua Protection Sun Gel",
+      price: "$33.00",
+      capacity: "Capacity and weight: SPF45 PA++++  50g (1.69 fl. oz.)",
+      size: {
+        g: "50g",
+        oz: "1.69 fl. oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Aloe vera leaf extract and Althaea rosea root extract help to form a cool moisture barrier.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Non sticky refreshing skin shield",
+          2: "Flawless triple skin care",
+          3: "Safe for sensitive skin",
+          4: "UV Protection",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+  {
+    id: 15,
+    data: {
+      img: null,
+      brand: "Incellderm",
+      productName: "Incellderm Active Cream EX",
+      price: "$66.00",
+      capacity: "Capacity and weight: 50ml (1.69 fl. oz.) ",
+      size: {
+        ml: "50ml",
+        oz: "1.69 fl. oz.",
+      },
+      skinType: "For all skin types",
+      descriptions: {
+        primary: {
+          primaryDescription:
+            "Incellderm Active Cream EX is a dual function face cream that corrects visible signs of aging and delivers effective skin brightening.",
+          secondaryDescription: null,
+          disclaimer: null,
+        },
+        secondary: {
+          1: "Regenerating and elasticity restoring",
+          2: "Anti-aging, wrinkle improving",
+          3: "Brightening",
+          4: "Rejuvenating",
+        },
+        generalWarnings: {
+          1: "Contains no harmful chemical ingredients",
+          2: "Suitable for all skin types, especially good for dry, thin, mature",
+          3: "For External use only",
+          4: "Not tested on Animals",
+          5: "US FDA approved",
+          shelfLife:
+            "3 years after the manufacture date, 6 months after opening.",
+        },
+      },
+    },
+  },
+];
