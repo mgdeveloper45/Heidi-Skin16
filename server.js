b5c1cd28a10248c940689329191eda1bb594bff4
seenanadimi <78619195+seenanadimi@users.noreply.github.com>
2022-04-25 18:55:45 -0700

    clean up

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const apiRoutes = require("./routes/api-routes");
+const stripeRoutes = require("./routes/stripe-routes");
 const PORT = process.env.PORT;
 
 if (process.env.NODE_ENV === "production") {
@@ -11,8 +13,6 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const apiRoutes = require("./routes/api-routes");
-const stripeRoutes = require("./routes/stripe-routes");
 app.use(apiRoutes);
 app.use(stripeRoutes);
 
