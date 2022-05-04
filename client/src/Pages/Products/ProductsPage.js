7fb6dd951d0c7c105ade8ae36270dd80a931cb2d
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-05-04 16:56:45 -0700

    Refactor(Products): Moved item to own component

--- client/src/Pages/Products/ProductsPage.js
+++ client/src/Pages/Products/ProductsPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
-import { addToCart } from '../../Redux/cartSlicer.js';
+
+//components
+import SingleProduct from './SingleProduct';
 
 const ProductWrapper = styled.div`
   display: flex;
@@ -35,74 +37,9 @@ const ProductGrid = styled.div`
   column-gap: 20px;
   row-gap: 20px;
 `;
-const ProductCard = styled.div`
-  height: 400px;
-  width: 100%;
-
-  display: flex;
-  flex-flow: column;
-  justify-content: space-around;
-`;
-
-const ImgContainer = styled.div`
-  width: 100%;
-
-  display: flex;
-  justify-content: center;
-`;
-const ProductImg = styled.img`
-  width: 100%;
-
-  min-width: 200px;
-
-  max-width: 200px;
-  max-height: 200px;
-`;
-const ProductDescription = styled.div`
-  display: flex;
-  flex-flow: column;
-`;
-
-const ProductName = styled.div``;
-const ProductPrice = styled.div``;
-const ProductDetails = styled.div`
-  display: flex;
-  flex-flow: column;
-`;
-
-const CartButton = styled.button`
-  height: 50px;
-  width: 75%;
-  border-radius: 10px;
-  box-shadow: 5px 5px 5px grey;
-  background-color: #e1b4bd;
-  border: none;
-  font-weight: bold;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:active {
-    transform: translateY(1.5px) translateX(1.5px);
-  }
-`;
 
 const ProductsPage = () => {
   const products = useSelector(state => state.productData.entities);
-
-  const dispatch = useDispatch();
-
-  const [num, setNum] = React.useState(0);
-  let incNum = () => {
-    setNum(num + 1);
-  };
-  let decNum = () => {
-    setNum(num - 1);
-  };
-  let handleChange = e => {
-    setNum(e.target.value);
-  };
 
   return (
     <ProductWrapper>
@@ -110,32 +47,7 @@ const ProductsPage = () => {
         <ProductDropdown>my boys</ProductDropdown>
         <ProductGrid>
           {products?.map((item, index) => (
-            <ProductCard key={index} item={item}>
-              <ImgContainer>
-                <ProductImg src='https://picsum.photos/400' alt='Product' />
-              </ImgContainer>
-              <ProductDescription>
-                <ProductName>{item.data.name}</ProductName>
-                <ProductPrice>
-                  <div>{item.data.metadata.price}</div>
-                  <div>
-                    <input type='text' value={num} onChange={handleChange} />
-                    <button onClick={incNum}>+</button>
-                    <button onClick={decNum}>-</button>
-                  </div>
-                </ProductPrice>
-                <ProductDetails>
-                  <div>{item.data.metadata.size}</div>
-                  <div>{item.data.description}</div>
-                </ProductDetails>
-              </ProductDescription>
-              <CartButton
-                onClick={() =>
-                  dispatch(addToCart({ ...products[index], quantity: 1 }))
-                }>
-                ADD TO CART
-              </CartButton>
-            </ProductCard>
+            <SingleProduct key={index} item={item} />
           ))}
         </ProductGrid>
       </ProductBody>
