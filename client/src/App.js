2791dbbe06fc7d493d502808a328c5e548e8b0bf
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2022-05-05 11:43:33 -0700

    feat(cart): create cart page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Contact from './Contact/Contact';
-import Footer from './Landing/Footer';
-import LandingPage from './Landing/LandingPage';
-import SalonServices from './Pages/SalonServices';
-import ProductsPage from './Pages/Products/ProductsPage';
-import Nav from './Nav/Nav';
-import Policy from './Pages/Policy/Policy';
-import Covid from './Pages/Covid/Covid';
-import './appstyles.css';
-import { useDispatch } from 'react-redux';
-import { fetchProducts } from './Redux/productSlice';
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Contact from "./Contact/Contact";
+import Footer from "./Landing/Footer";
+import LandingPage from "./Landing/LandingPage";
+import SalonServices from "./Pages/SalonServices";
+import ProductsPage from "./Pages/Products/ProductsPage";
+import Nav from "./Nav/Nav";
+import Policy from "./Pages/Policy/Policy";
+import Covid from "./Pages/Covid/Covid";
+import Cart from "./Pages/Cart/Cart";
+import "./appstyles.css";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "./Redux/productSlice";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,12 +24,12 @@ function App() {
   // animate open if closed, redirects if open
   const animateImg = () => {
     if (visible === true) {
-      console.log('This is when I go to another page');
+      console.log("This is when I go to another page");
     } else {
-      const animatedImg = document.querySelector('.rightImg');
-      animatedImg.classList.add('rightBox');
-      animatedImg.classList.remove('rightBoxes');
-      animatedImg.style.marginRight = '50%';
+      const animatedImg = document.querySelector(".rightImg");
+      animatedImg.classList.add("rightBox");
+      animatedImg.classList.remove("rightBoxes");
+      animatedImg.style.marginRight = "50%";
       setVisible(true);
     }
   };
@@ -36,13 +37,13 @@ function App() {
   const animateRevImg = () => {
     return new Promise((resolve, reject) => {
       if (visible === true) {
-        const animatedImg = document.querySelector('.rightImg');
-        animatedImg.classList.remove('rightBox');
-        animatedImg.classList.add('rightBoxes');
-        animatedImg.style.marginRight = '0px';
+        const animatedImg = document.querySelector(".rightImg");
+        animatedImg.classList.remove("rightBox");
+        animatedImg.classList.add("rightBoxes");
+        animatedImg.style.marginRight = "0px";
         setTimeout(() => {
           setVisible(false);
-          resolve('finished');
+          resolve("finished");
         }, 1000);
       }
     });
@@ -52,14 +53,15 @@ function App() {
       <Nav visible={visible} animateRevImg={animateRevImg} />
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={<LandingPage animate={animateImg} visible={visible} />}
         />
-        <Route path='/services' element={<SalonServices />} />
-        <Route path='/products' element={<ProductsPage />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/policy' element={<Policy />} />
-        <Route path='/covid' element={<Covid />} />
+        <Route path="/services" element={<SalonServices />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/covid" element={<Covid />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
       <Footer />
     </>
