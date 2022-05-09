c878a0a30c3652838724812cfd78d5d423f4971e
Akiko Green <agreen@alphaworks.tech>
2022-05-09 14:29:48 -0700

    feat(About):

--- client/src/About/About.js
+++ client/src/About/About.js
@@ -1,10 +1,48 @@
-import React from 'react'
-
+import React from "react";
+import {
+  AboutContainer,
+  AboutHeader,
+  AboutImageContainer,
+  AboutImage,
+  ParagraphContainer,
+  Paragraph,
+  Span
+} from "./AboutStyles";
+import "./About.css";
 const About = () => {
   return (
-    <div>About Us</div>
-    
-  )
-}
+    <AboutContainer>
+      <AboutHeader>About Us</AboutHeader>
+      <AboutImageContainer>
+        <h3 className="about-live">Live</h3>
+        <AboutImage></AboutImage>
+        <h3 className="beauty">Beautifully</h3>
+      </AboutImageContainer>
+      <Span>HEIDI</Span>SKIN16
+      <ParagraphContainer>
+        <Paragraph>
+          Enter Owner bio and the mission statement of the company. Can use a
+          picture of estetician at work with real clients above, just make sure
+          it’s a singular picture that represents your company or products well.
+          State your expertise in the field, the products and services you are
+          known for.
+        </Paragraph>
+        <hr />
+        <Paragraph>
+          Mention how long you have been in the industry What cities and
+          counties have you served. Is there anything personal you want your
+          customers to know about why you created these products or services. Do
+          you have a target demographic.{" "}
+        </Paragraph>
+        <hr />
+        <Paragraph>
+          Other esteticians who work at the salon, if any, can be mentioned
+          here. Any Additional info.
+        </Paragraph>
+        <hr/>
+      </ParagraphContainer>
+    </AboutContainer>
+  );
+};
 
-export default About
+export default About;
---
+++ client/src/About/AboutStyles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const AboutContainer = styled.div`
+  border: 2px solid green;
+`;
+
+export const AboutHeader = styled.h1`
+  text-align: center;
+  margin-bottom: 1em;
+  /* width: 552px;
+  height: 98px; */
+`;
+
+export const AboutImageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+`;
+export const AboutImage = styled.img`
+  background-color: blue;
+  height: 925px;
+  width: 839px;
+  border-radius: 10px;
+`;
+
+export const Span = styled.span`
+  font-weight: 200;
+  font-family: Helvetica Neue;
+  font-style: normal;
+  font-size: 60px;
+`;
+
+export const ParagraphContainer = styled.div`
+
+`
+
+export const Paragraph = styled.p`
+  padding:10px ;
+  font-size:2em ;
+  line-height: 60px;
+  font-family: "HelveticaNeue-Light", sans-serif;
+`
+
+
