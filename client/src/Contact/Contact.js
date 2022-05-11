8cf00f433f27ab8419291e7eddcf85edd66abcea
MarkGee45 <moel55.mg@gmail.com>
2022-05-11 11:24:00 -0700

    change(email): changed email service from emailjs to formsubmit

--- client/src/Contact/Contact.js
+++ client/src/Contact/Contact.js
@@ -1,5 +1,4 @@
-import React, { useRef }  from 'react';
-import emailjs from '@emailjs/browser';
+import React from 'react';
 import Addresss from '../Landing/Address/Addresss';
 import { 
     Button, Confirm, Disclaimer, EmailInput, 
@@ -9,42 +8,25 @@ import {
 from './ContactStyles';
 
 const Contact = () => {
-    // const service_id = process.env.SERVICE_ID
-    // const template_id = process.env.TEMPLATE_ID
-    // const public_key = process.env.PUBLIC_KEY
-    const form = useRef();
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm("service_grefs4n", "template_bejxi1n", e.target, "Ufc9dff3XBSBnpYcl")
-        // emailjs.sendForm("service_id", "template_id", e.target, "public_key")
-        .then((result) => {
-            console.log(result);
-        }, (error) => {
-            console.log(error);
-        });
-        e.target.reset();
-    }
     return (
         <>
         <FormContainer>
             <H2>Get in Touch!</H2>
-            <Form ref={form} onSubmit={sendEmail}>
+            <Form action="https://formsubmit.co/heidiskin16@gmail.com" target="_blank" method="POST">
                 <Info> 
                     <Label>Name: </Label>
                     <Label>Email:</Label>
                     <Label>Message: </Label>
                 </Info>
                 <FormInput>
-                    <Input type="text" name="user_name"/>
-                    <EmailInput type="email" name="user_email"/>
+                    <Input type="text" name="name"/>
+                    <EmailInput type="email" name="email"/>
                     <TextArea name="message"/>
                     <Confirm>
                         <Disclaimer>
                             <P>* Service By Appointment Only</P>
                         </Disclaimer>
-                        <Button type="submit" value="Send">
+                        <Button type="submit" value="Submit">
                             Submit
                         </Button>
                     </Confirm>
