49f069026c732cdf32ed0878ef6e95dad096b585
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-07-12 14:44:08 -0700

    Edit(Contact): reconstructed contact sheet

--- client/src/Contact/ContactStyles.js
+++ client/src/Contact/ContactStyles.js
@@ -1,37 +1,44 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.div`
+  width: min(100% - 2rem, 960px);
+  margin: auto;
   display: flex;
   flex-direction: column;
   justify-content: center;
 `;
 export const Form = styled.form`
   display: flex;
-  justify-content: space-around;
+  flex-flow: column;
 `;
 export const FormInput = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: flex-start;
-  height: 1192px;
-  width: 1397px;
   background-color: var(--pink);
   border-radius: 20px 20px 0px 0px;
+  padding-top: 50px;
 `;
 export const Info = styled.div`
-  height: 700px;
+  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
+  align-items: center;
+`;
+
+export const InfoLine = styled.div`
+  width: 85%;
+  display: flex;
+  margin: 25px 0;
 `;
 export const H2 = styled.h2`
-  width: 551px;
-  height: 198px;
   font-style: normal;
   font-weight: normal;
   font-size: 60px;
   align-self: center;
+  margin-bottom: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -41,10 +48,10 @@ export const Label = styled.label`
   height: 64px;
   font-style: normal;
   font-weight: 400;
-  font-size: 35px;
+  font-size: 24px;
 `;
 export const Input = styled.input`
-  width: 573px;
+  width: 30%;
   height: 64px;
   margin-left: 50px;
   background-color: var(--white);
@@ -54,7 +61,7 @@ export const Input = styled.input`
   font-size: 35px;
 `;
 export const EmailInput = styled.input`
-  width: 859px;
+  width: 60%;
   height: 64px;
   margin-left: 50px;
   background-color: var(--white);
@@ -64,7 +71,7 @@ export const EmailInput = styled.input`
   font-size: 35px;
 `;
 export const TextArea = styled.textarea`
-  width: 1126px;
+  width: 85%;
   height: 271px;
   margin-left: 50px;
   background-color: var(--white);
@@ -74,23 +81,24 @@ export const TextArea = styled.textarea`
   font-size: 35px;
 `;
 export const Confirm = styled.div`
+  width: 100%;
   display: flex;
-  justify-content: space-around;
-  width: 70vw;
+  justify-content: center;
+  padding: 50px 0px;
 `;
 export const Disclaimer = styled.div`
-  width: 410px;
-  height: 42px;
+  width: 85%;
+  display: flex;
+  justify-content: space-between;
 `;
 export const Button = styled.button`
-  width: 307px;
-  height: 98px;
+  padding: 15px;
   background-color: var(--green);
   box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.5);
   border-radius: 20px;
   font-style: normal;
   font-weight: bold;
-  font-size: 55px;
+  font-size: 35px;
   text-align: center;
   color: var(--white);
 `;
